9ea843211ee170f2fd15e75e32be9575565dd1de
Amanda Allred <amanda.barnett11@yahoo.com>
2026-03-12 11:30:51 -0400

    pseudocode

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a deliverable for the January 2026 Data Analysis cohort for Code:You.
+
+It's aim is to create a Python program that plays a game of Rock, Paper, Scissors between a user and the computer. You’ll practice handling user input, generating random selections, and applying conditional logic.
---
+++ python_rps_game.py
@@ -0,0 +1,28 @@
+# User input (r/p/s)
+
+# Convert input to lowercase
+
+# Validate entry (r/p/s)
+    # Compare to list of r/p/s
+
+# if match, continue
+# if not, error message
+
+# computer play is randomized from list
+
+# if comp & input are same: print tie
+
+# if comp or input = r & comp or input = s
+    # if comp = r, comp wins
+    # else input wins
+
+# if comp or input = s & comp or input = p
+    # if comp = s, comp wins
+    # else input wins
+
+# if comp or input = p & comp or input = r
+    # if comp = p, comp wins
+    # else input wins
+
+# print both choices
+# print who won / tie
